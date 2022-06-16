a17109550abfed55775417f39e73c5a9867715df
d.prokofev <prokofiev.pub@gmail.com>
2022-06-17 02:00:55 +0300

    update

--- install.php
+++ install.php
@@ -1,4 +1,12 @@
 <?php
 
 use Glossary\Glossary;
+
+\Cetera\Application::getInstance()->getConn()->executeQuery("CREATE TABLE IF NOT EXISTS g_list_plugin 
+(id int(11) NOT NULL, 
+term varchar(60), 
+specification varchar(500), 
+synonyms varchar(500), 
+links varchar(1000))");
+
 Glossary::inint();
